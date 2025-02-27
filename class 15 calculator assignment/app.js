936e3d723ab7a5969c3f0817d9ef10e08cf71725
Talha Habib <talhahabibofficial.18@gmail.com>
2025-02-27 01:25:46 -0800

    Add files via upload

---
+++ class 15 calculator assignment/app.js
@@ -0,0 +1,19 @@
+function btnClick(a){
+    var getInp = document.getElementById("inp")
+    getInp.value += a
+}
+
+function eq(){
+     var getInp = document.getElementById("inp");
+     getInp.value = eval(getInp.value);
+}
+
+function clrall() {
+  var getInp = document.getElementById("inp");
+  getInp.value = "";
+}
+
+function clr() {
+  var getInp = document.getElementById("inp");
+  getInp.value = getInp.value.slice(0, -1);
+}
